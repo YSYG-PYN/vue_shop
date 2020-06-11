fe5d48990c0ebdaeadeafcf410186e6786daf460
YSYG-PYN <1755684077@qq.com>
2020-06-11 23:13:14 +0800

    完成用户列表

---
+++ src/router/index.js
@@ -0,0 +1,54 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import login from '../components/login'
+import home from '../components/home'
+import welcome from '../components/welcome'
+import Users from '../components/user/Users'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    component: login
+  },
+  {
+    path: '/home',
+    component: home,
+    redirect: '/welcome',
+    children: [
+      {
+        path: '/welcome',
+        component: welcome
+      },
+      {
+        path: '/users',
+        component: Users
+      }
+    ]
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+// 挂在路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 代表从哪个路径条状而来
+  // next 是一个函数，表示放行
+  // next() 放行 next('/login') 强制跳转
+
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
+
+export default router
